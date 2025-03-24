a4c21b09fdf48b8d2336d93f5326201d0aa9ec1d
sandeep0009 <500sandeepks@gmail.com>
2025-03-24 19:50:31 +0530

    fix:fix deployment issues

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,7 +5,7 @@ export function Navbar() {
     <div className="p-6">
       <div className="flex flex-col sm:flex-row items-center justify-between">
         <div className="max-w-lg text-center sm:text-left">
-          <h2 className="text-5xl font-bold">Hi, I'm Sandeep</h2>
+          <h2 className="text-5xl font-bold">Hi, I&apos;m Sandeep</h2>
           <p className="mt-2 text-gray-500 py-6">
             A passionate Full Stack Developer dedicated to building scalable and efficient web applications. 
             I thrive on solving complex problems and actively contribute 
